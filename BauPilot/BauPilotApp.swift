4854097a2ae820316b44754d0f41fb68fc165b0b
Marcial <marcialreichert@googlemail.com>
2026-03-28 22:13:01 +0100

    Initial Commit

---
+++ BauPilot/BauPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BauPilotApp.swift
+//  BauPilot
+//
+//  Created by Marcial Reichert on 28.03.26.
+//
+
+import SwiftUI
+
+@main
+struct BauPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
